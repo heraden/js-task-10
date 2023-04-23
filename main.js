859103f55d312a82670d9f2ba13d815790743d96
heraden <127416579+heraden@users.noreply.github.com>
2023-04-23 18:42:42 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-function calculateDepositAmount() { 
-      const initialAmount =parseInt (prompt("Enter the initial amount: ")); 
-       const durationInYears = parseInt(prompt("Enter the duration in years: ")); 
-       const annualInterestRate = parseInt(prompt("Enter the annual interest rate in %: ")); 
-     
-       let depositAmount = initialAmount; 
-       for (let i = 0; i < durationInYears; i++) { 
-       const interestAmount = depositAmount * annualInterestRate / 100; 
-         depositAmount += interestAmount; 
-       } 
-     
-      console.log( depositAmount); 
-     } 
-     calculateDepositAmount()
+document.getElementById("bottom").addEventListener("click", function () { 
+    event.preventDefault(); 
+    let amount = parseFloat(document.getElementById("amount").value) 
+    let years = parseFloat(document.getElementById("years").value )
+    let rate1 =parseFloat( document.getElementById("interest").value) 
+   
+    let amount1 = amount; 
+    for (let i = 0; i < years; i++) { 
+      const lastAmount = amount1 *rate1 / 100; 
+      amount1 += lastAmount; 
+    } 
+   
+    return console.log( amount1); 
+  })
